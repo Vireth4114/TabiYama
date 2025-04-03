90c693935418deeb8e793de62b56bf94c501a5db
aubry224 <aubry224@unicaen.fr>
2025-04-03 11:47:42 +0200

    Feat: profile and basic content

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -1,13 +1,16 @@
+import { Person } from "@mui/icons-material";
 import React from "react";
 export default function Header() {
   return (
     <>
-      <div className='position-absolute top-0 left-0 w-100 bg-primary h-40px d-flex align-items-center'>
-        <strong className='text-white p-4'>
-          TabiYama
+      <div className='position-absolute row top-0 start-0 w-100 bg-primary h-80px d-flex align-items-center'>
+        <strong className='text-white p-4 h2 col'>
+          MountBox
         </strong>
+        <div className="col-8"></div>
+        <Person fontSize="large" className="col text-white h2"/>
       </div>
-      <div className='h-40px'></div>
+      <div className='h-80px'></div>
     </>
   );
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import { Button } from '@mui/material';
+
 export default function HomePage() {
   let navigate = useNavigate();
   const routeChange = () => {
     navigate('/other');
   }
   return (
-    <button onClick={routeChange}>Truc</button>
+    <page class='p-5'>
+      <section>
+        <h2>Viver la montagne, les yeux fermés !</h2>
+        <div>
+          Avec MountBox, osez la montagne, c'est un sacré banger de Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, quibusdam magni, quisquam impedit provident aspernatur tempore aliquam dolores ut nobis, laudantium dolor perspiciatis reiciendis omnis obcaecati quasi ex sapiente blanditiis.
+        </div>
+      </section>
+    </page>
   );
 }
